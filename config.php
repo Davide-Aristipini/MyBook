7e34564ebd691b651cbdc6366e7afccbe695dd15
Davide-Aristipini <68905819+Davide-Aristipini@users.noreply.github.com>
2023-07-28 12:22:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
 session_start();
 
-$connect = mysqli_connect("localhost", "hsyefxis_venditori2", "Casafamiglia123", "hsyefxis_esercitazione");
+$connect = mysqli_connect("localhost", "user", "pass", "db");
 ?>
